e365912f83a7adbc50392aaac98b9d0e773756f5
arturhasahanov <arturbk2@yandex.ru>
2022-07-31 08:45:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import wikiModel, telebot
+
+
+# Создаем экземпляр бота
+bot = telebot.TeleBot('5366147447:AAHszjrtM0bHWKALS_2mVTWQIZOb6SpxIpA')
+#keys = telebot.types.ReplyKeyboardMarkup(True, True)
+
+#keys.row('Русский', 'Английский')
+
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(message, res=False):
+    bot.send_message(message.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
+    #bot.send_message(message.chat.id, 'Выбери язык запроса:', reply_markup=keys)
+    #wikiModel.set_lang(message.text)
+
+
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, wikiModel.getwiki(message.text)[0] + wikiModel.getwiki(message.text)[1])
+
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
---
+++ wikiModel.py
@@ -0,0 +1,55 @@
+import wikipedia, re, string
+
+# Устанавливаем русский язык в Wikipedia
+wikipedia.set_lang("ru")
+
+alphabet = string.ascii_lowercase + 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя' + string.digits
+
+def set_lang(message):
+    if message == 'Русский':
+        wikipedia.set_lang("ru")
+    elif message == 'Английский':
+        wikipedia.set_lang("en")
+
+def clean_str(mesage):
+    res = mesage.lower()
+    res = [c for c in res if c in alphabet]
+    return ''.join(res)
+
+
+
+def getwiki(s):
+    try:
+        clear_text = clean_str(s)
+        ny = wikipedia.page(clear_text)
+        # Получаем первую тысячу символов(можно взять больше)
+        wikitext = ny.content[:1000]
+        wikilinks = ny.url
+        # Разделяем по точкам ( потому что по точкам прозе всего )
+        wikimas = wikitext.split('.')
+        # Отбрасываем все после последней точки
+        wikimas = wikimas[:-1]
+        # Создаем пустую переменную для текста
+        wikitext2 = ''
+        # Проходимся по строкам, где нет знаков «равно» (то есть все, кроме заголовков)
+        for x in wikimas:
+            if not ('==' in x):
+                # Если в строке осталось больше трех символов, добавляем ее к нашей переменной и возвращаем утерянные при разделении строк точки на место
+                if (len((x.strip())) > 3):
+                    wikitext2 = wikitext2 + x + '.'
+            else:
+                break
+        # Теперь при помощи регулярных выражений убираем разметку( можно и без этого но на всякий случай)
+        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
+        wikitext2 = wikitext2 + '\n\n'
+        # Возвращаем текстовую строку
+        return wikitext2, wikilinks
+    # Обрабатываем исключение, которое мог вернуть модуль wikipedia при запросе
+    except Exception as e:
+        return 'В энциклопедии нет информации об этом\n', 'Введите что-нибудь другое\n'
+
+
+if __name__ == "__main__":
+    print(getwiki('Чайнатаун'))
